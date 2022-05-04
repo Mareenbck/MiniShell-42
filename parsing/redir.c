a900dfa6e8bdd76b6035269d9a95d51ee20fe95b
ecariot <emm.cariot@gmail.com>
2022-05-04 17:16:44 +0200

    parser redir et pipe

--- parsing/redir.c
+++ parsing/redir.c
@@ -6,7 +6,7 @@
 /*   By: emcariot <emcariot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/03 19:53:14 by emcariot          #+#    #+#             */
-/*   Updated: 2022/05/04 14:58:52 by emcariot         ###   ########.fr       */
+/*   Updated: 2022/05/04 16:36:04 by emcariot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	check_redir_o_position(t_token *token, t_cmd *cmd)
 	if (token->next->token != WORD && token->next != NULL)
 		ft_error("error syntax 2\n");
 	if (token->prev == NULL)
-		ft_putstr_fd("error syntax \n", 2);
+		ft_error("error syntax \n");
 	return (0);
 }
 
@@ -28,6 +28,16 @@ int	check_redir_i_position(t_token *token, t_cmd *cmd)
 	if (token->next->token != WORD && token->next != NULL)
 		ft_error("error syntax 2\n");
 	if (token->prev == NULL)
-		ft_putstr_fd("error syntax \n", 2);
+		ft_error("error syntax \n");
+	return (0);
+}
+
+int	check_pipe_position(t_token *token, t_cmd *cmd)
+{
+	(void)cmd;
+	if (token->next->token != WORD && token->next != NULL)
+		ft_error("error syntax 1\n");
+	if (token->prev == NULL)
+		ft_error("error syntax\n");
 	return (0);
 }
