73d486b0341168b0b48ed9924c79f1e8779eb8d8
ecariot <emm.cariot@gmail.com>
2022-05-10 11:42:06 +0200

    trim bien quand il faut pour les quotes

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: emcariot <emcariot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 17:35:25 by emcariot          #+#    #+#             */
-/*   Updated: 2022/05/10 10:44:02 by emcariot         ###   ########.fr       */
+/*   Updated: 2022/05/10 11:37:59 by emcariot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -76,19 +76,19 @@ typedef struct s_global
 }	t_global;
 
 /* PARSING */
-int	count_option(char *line);
-void	analize_cmd(t_token **head, t_cmd **comd);
-void	ft_print_cmd(t_cmd **cmd);
-t_cmd *ft_init_cmd();
+int			count_option(char *line);
+void		analize_cmd(t_token **head, t_cmd **comd);
+void		ft_print_cmd(t_cmd **cmd);
+t_cmd		*ft_init_cmd();
 
 // PIPE
-int	check_pipe_position(t_token *token, t_cmd *cmd);
+int			check_pipe_position(t_token *token, t_cmd *cmd);
 
 //REDIR
-int check_redir_o_position(t_token *token, t_cmd *cmd);
-int check_redir_i_position(t_token *token, t_cmd *cmd);
-int check_append_o(t_token *token, t_cmd *cmd);
-int check_append_i(t_token *token, t_cmd *cmd);
+int			check_redir_o_position(t_token *token, t_cmd *cmd);
+int			check_redir_i_position(t_token *token, t_cmd *cmd);
+int			check_append_o(t_token *token, t_cmd *cmd);
+int			check_append_i(t_token *token, t_cmd *cmd);
 
 //QUOTE - TRIM
 int			is_doble_quotes(char c);
@@ -97,8 +97,10 @@ void		trim_doble_quotes(t_token *token);
 void		trim_simple_quotes(t_token *token);
 
 //QUOTE - PARSE
-int			check_nb_d_quotes(t_token *token);
-int     check_nb_s_quotes(t_token *token);
+int			count_d_quotes(t_token *token);
+int			count_s_quotes(t_token *token);
+void		recup_count_d_quotes(t_token *token);
+void		recup_count_s_quotes(t_token *token);
 
 /* UTILS */
 void	**ft_free_tab(char **tab);
--- parsing/parse_quotes.c
+++ parsing/parse_quotes.c
@@ -6,13 +6,13 @@
 /*   By: emcariot <emcariot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/06 13:26:05 by emcariot          #+#    #+#             */
-/*   Updated: 2022/05/10 10:43:16 by emcariot         ###   ########.fr       */
+/*   Updated: 2022/05/10 11:40:27 by emcariot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int	check_nb_d_quotes(t_token *token)
+int	count_d_quotes(t_token *token)
 {
 	int	i;
 	int count;
@@ -27,13 +27,12 @@ int	check_nb_d_quotes(t_token *token)
 				count++;
 			i++;
 		}
-		printf("count = %d\n", count);
 		token = token->next;
 	}
 	return (count);
 }
 
-int	check_nb_s_quotes(t_token *token)
+int	count_s_quotes(t_token *token)
 {
 	int	i;
 	int count;
@@ -48,8 +47,29 @@ int	check_nb_s_quotes(t_token *token)
 				count++;
 			i++;
 		}
-		printf("count = %d\n", count);
 		token = token->next;
 	}
 	return (count);
+}
+
+void	recup_count_d_quotes(t_token *token)
+{
+	int countd;
+
+	countd = count_d_quotes(token);
+	if (countd % 2 == 0)
+		trim_doble_quotes(token);
+	else
+		ft_error("Command not found\n");
+}
+
+void	recup_count_s_quotes(t_token *token)
+{
+	int counts;
+
+	counts = count_s_quotes(token);
+	if (counts % 2 == 0)
+		trim_simple_quotes(token);
+	else
+		ft_error("Command not found");
 }
