bd9a0b95913dc1369fee7261d6e200223a8037f6
Emma CARIOT <emcariot@e1r5p4.clusters.42paris.fr>
2022-05-24 16:51:39 +0200

    manque le redir in error

--- append/redir.c
+++ append/redir.c
@@ -6,7 +6,7 @@
 /*   By: emcariot <emcariot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/20 12:28:11 by emcariot          #+#    #+#             */
-/*   Updated: 2022/05/24 16:04:52 by emcariot         ###   ########.fr       */
+/*   Updated: 2022/05/24 16:30:08 by emcariot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,9 @@ int	redir_in(t_cmd *cmd, char *file_name)
 	fd = open(file_name, O_RDONLY, 0644);
 	if (fd == -1)
 	{
-		perror(file_name);
-		return (EXIT_FAILURE);
+		//perror(file_name);
+		//return (EXIT_FAILURE);
+		return (1);
 	}
 	if (cmd->input != STDIN_FILENO)
 	{
--- parsing/redir_syntax.c
+++ parsing/redir_syntax.c
@@ -6,23 +6,13 @@
 /*   By: emcariot <emcariot@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/18 10:55:28 by emcariot          #+#    #+#             */
-/*   Updated: 2022/05/24 16:04:15 by emcariot         ###   ########.fr       */
+/*   Updated: 2022/05/24 16:41:27 by emcariot         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
 int	check_redir_o_position(t_token *token, t_cmd *cmd)
-{
-	(void)cmd;
-	if (token->next->token != WORD && token->next != NULL)
-		return (1);
-	if (token->prev == NULL)
-		return (1);
-	return (0);
-}
-
-int	check_redir_i_position(t_token *token, t_cmd *cmd)
 {
 	(void)cmd;
 	if (token->next->token != WORD && token->next != NULL)
@@ -50,16 +40,12 @@ int check_heredoc(t_token *token, t_cmd *cmd)
 	return (0);
 }
 
-// void	analize_redir(t_token *token, t_cmd *cmd)
-// {
-// 	if (token->token == REDIR_OUT)
-// 	{
-// 		check_redir_o_position(token, cmd);
-// 		redir_out(cmd, token->next->val);
-// 	}
-// 	if (token->token == REDIR_IN)
-// 	{
-// 		//check_redir_i_position(token, cmd);
-// 		redir_in(cmd, token->prev->val);
-// 	}
-// }
+int	check_redir_i_position(t_token *token, t_cmd *cmd)
+{
+	(void)cmd;
+	if (token->next->token != WORD && token->next != NULL)
+		return (1);
+	if (token->prev == NULL)
+		return (1);
+	return (0);
+}
